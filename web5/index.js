d526892d641f3e4ba278ceb82893618fac7837d9
thexovc <osazeepeter79@gmail.com>
2023-12-06 13:45:07 +0100

    dwn crud

--- web5/index.js
+++ web5/index.js
@@ -7,4 +7,35 @@ if (!globalThis.crypto) globalThis.crypto = webcrypto;
 
 const { web5, did: aliceDid } = await Web5.connect();
 
-console.log(aliceDid);
+// console.log(aliceDid);
+
+const { record: greet } = await web5.dwn.records.create({
+  data: "Hello, Web5",
+  message: {
+    dataFormat: "text/plain",
+  },
+});
+
+const { record: username } = await web5.dwn.records.create({
+  data: "kyilax",
+  message: {
+    dataFormat: "text/plain",
+  },
+});
+
+// console.log({ record });
+
+// Update DWN Records
+await greet.update({
+  data: "Hello, Web5! I am updated.",
+});
+
+const readRecord = await greet.data.text();
+
+// delete dwn
+await greet.delete();
+
+const readUsername = await username.data.text();
+
+console.log({ readRecord });
+console.log({ readUsername });
